7988998c5e6c6e29e3a17bfe11c651826867c398
Elizabeth Martin <mothereffingsufferage@gmail.com>
2021-03-18 12:36:42 -0400

    Eating my own dog food

--- fish/.config/fish/functions/git_open.fish
+++ fish/.config/fish/functions/git_open.fish
@@ -2,8 +2,16 @@
 
 function git_open
     if git remote get-url --all origin > /dev/null ^ /dev/null
-        set -l REPOSITORY (git remote get-url --all origin | sed 's/git@//' | sed 's/:/\//')
-        xdg-open $REPOSITORY
+        if git remote get-url --all origin | grep -q "github.com\|gitlab.com\|bitbucket.org\|gitea.com"
+            if git remote get-url --all origin | grep -q "https"
+                set REPOSITORY (git remote get-url --all origin)
+            else
+                set REPOSITORY (git remote get-url --all origin | sed 's/git@//' | sed 's/:/\//')
+            end
+            xdg-open $REPOSITORY
+        else
+            echo "ERROR: Repository not on a supported host"
+        end
     else
         echo "ERROR: You are not in a git repository"
     end
